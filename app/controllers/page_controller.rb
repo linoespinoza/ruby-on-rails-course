cad81b1f86c1c4767f0dbf2b923071e1d75eff89
Lino Espinoza <woztheman@gmail.com>
2015-02-21 13:37:30 -0500

    Se crean view helpers

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -12,6 +12,8 @@ class PageController < ApplicationController
   end
 
   def calendar
+    @year = params[:year]
+    @month = params[:month]
     @name = "Lino Espinoza"
     @my_time = Time.now
     @products = %w(Skiis boots poles mittens) # = ["Skiis", "boots", "poles", "mittens"]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def calendar(month, year)
+    prev_month = link_to "Previous", page_calendar_path(:month => month.to_i - 1, :year => year) 
+    cal_str = "<table border=1>"
+    cal_str = "<tr><td>#{prev_month}</td><td colspan='7'>October</td></tr>"
+    cal_str += "<tr>"
+    cal_str += "<td>&nbsp;</td>"
+    6.times do |day|
+      cal_str += "<td>#{day + 1}</td>"
+    end
+    cal_str += "</tr>"
+    cal_str += "</table>"
+    cal_str.html_safe
+  end
+
 end
